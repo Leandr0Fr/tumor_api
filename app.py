de52c01bb49bd129a918797d7fa960ac3fa6bb7b
leandroFr <leandro00fr@gmail.com>
2023-10-06 16:11:14 -0300

    ADD endpoint ping

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "HOOOOLAAAA"
+    
+@app.route("/ping", methods=["GET"])
+def ping():
+    return jsonify(message='API on!'), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
